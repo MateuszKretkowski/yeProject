162cee9e54ad9e8bc6f58932013271368b73ae92
MateuszKretkowski <mkretkowski13@gmail.com>
2023-09-14 21:55:13 +0200

    fixed some bugs

--- App.js
+++ App.js
@@ -15,7 +15,8 @@ function App() {
       <header>
         <div className="header_container">
           <h1 className="header_title">kanYe West</h1>
-          <h4 className="header_subtitle">"If you're a fan of Ye, you're a fan of Yourself"</h4>
+          <h4 className="header_subtitle">" If you're a fan of <span className="text_glow cartoony">Ye</span>, you're a fan of <span className="text_glow cartoony">Yourself</span> "</h4>
+          {/* <div className="scroll" /> */}
         </div>
       </header>
     </div>
